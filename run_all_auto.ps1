6b735e6b50937196d9d1fe10f2f3a3dbbf07df80
huxiaoou <516984451@qq.com>
2021-11-02 15:41:18 +0800

    Change algorithm for the input of run_all_auto.ps1

--- run_all_auto.ps1
+++ run_all_auto.ps1
@@ -1,2 +1,6 @@
-$trade_date = Get-Date -Format yyyyMMdd
-python run_all.py $trade_date >> .\log\$trade_date.log
+$trade_date = Get-Date
+$stp_date = $trade_date.AddDays(1)
+$stp_date = $stp_date.ToString("yyyyMMdd")
+Write-host "trade_date = " $trade_date
+Write-host "stop_date  = " $stp_date
+python run_all.py $stp_date >> .\log\$trade_date.log
