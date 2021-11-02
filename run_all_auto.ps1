6f7e3bc3b3437204758ad9bc28553db2f723b461
huxiaoou <516984451@qq.com>
2021-11-02 16:28:40 +0800

    Change algorithm for the input of run_all_auto.ps1

--- run_all_auto.ps1
+++ run_all_auto.ps1
@@ -1,5 +1,6 @@
 $trade_date = Get-Date
 $stp_date = $trade_date.AddDays(1)
+$trade_date = $trade_date.ToString("yyyyMMdd")
 $stp_date = $stp_date.ToString("yyyyMMdd")
 Write-host "trade_date = " $trade_date
 Write-host "stop_date  = " $stp_date
