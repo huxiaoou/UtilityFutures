73f26e926fe1d9285c5b6093a04ff072b073e355
huxiaoou <516984451@qq.com>
2021-11-15 16:50:11 +0800

    Append more instruments. Fix some bugs.

--- run_all_manual.ps1
+++ run_all_manual.ps1
@@ -1,2 +1,2 @@
 $stp_date = Read-Host -Prompt "Please input the stp date, format = [YYYYMMDDD]"
-python run_all.py $stp_date >> .\log\$trade_date.log
+python run_all.py $stp_date >> .\log\manual.log
