0a0745d1c68016291611dc0d1834a20f70da6609
huxiaoou <516984451@qq.com>
2021-12-02 09:29:18 +0800

    Add sleep function to run_all.py to avoid a strange bug, which would miss executing some script.

--- run_all.py
+++ run_all.py
@@ -1,10 +1,14 @@
 import subprocess
 import sys
 from configure import CONCERNED_INSTRUMENT_UNIVERSE
+from time import sleep
 
 stp_date = sys.argv[1]
 
 for instrument_id in CONCERNED_INSTRUMENT_UNIVERSE:
+    sleep(1)
     subprocess.call(["python", "cal_0_major_minor.py", instrument_id, stp_date])
+    sleep(1)
     subprocess.call(["python", "cal_1_major_return.py", instrument_id])
+    sleep(1)
     subprocess.call(["python", "cal_2_reformat_md.py", instrument_id, stp_date])
