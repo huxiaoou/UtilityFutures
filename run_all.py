7312b5de44066911186ff65a2372bbd05fe025d0
huxiaoou <516984451@qq.com>
2021-12-10 10:28:24 +0800

    Still can not fix the strange bug cased by "subprocess.call" which would miss executing some script. Try to add another additional "sleep(1)" at the end of run-all loop.

--- run_all.py
+++ run_all.py
@@ -12,3 +12,4 @@ for instrument_id in CONCERNED_INSTRUMENT_UNIVERSE:
     subprocess.call(["python", "cal_1_major_return.py", instrument_id])
     sleep(1)
     subprocess.call(["python", "cal_2_reformat_md.py", instrument_id, stp_date])
+    sleep(1)
