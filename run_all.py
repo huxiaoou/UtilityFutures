be2abfc98f885157d80376d8cabe4bbfc2ab3c0b
huxiaoou <516984451@qq.com>
2021-12-10 10:56:15 +0800

    Change subprocess.call() to subprocess.run(), which is recommended best practise in since python 3.5.
    Will observe whether the previous bug still exists.

--- run_all.py
+++ run_all.py
@@ -5,11 +5,10 @@ from time import sleep
 
 stp_date = sys.argv[1]
 
-for instrument_id in CONCERNED_INSTRUMENT_UNIVERSE:
+for instrument_id in CONCERNED_INSTRUMENT_UNIVERSE[0:1]:
     sleep(1)
-    subprocess.call(["python", "cal_0_major_minor.py", instrument_id, stp_date])
+    subprocess.run(["python", "cal_0_major_minor.py", instrument_id, stp_date])
     sleep(1)
-    subprocess.call(["python", "cal_1_major_return.py", instrument_id])
+    subprocess.run(["python", "cal_1_major_return.py", instrument_id])
     sleep(1)
-    subprocess.call(["python", "cal_2_reformat_md.py", instrument_id, stp_date])
-    sleep(1)
+    subprocess.run(["python", "cal_2_reformat_md.py", instrument_id, stp_date])
