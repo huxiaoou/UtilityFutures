7de7ca9828e5d7d68ecc34b476324a146f2b4c50
huxiaoou <516984451@qq.com>
2022-07-15 11:01:44 +0800

    Add extra data basis.

--- cal_3_extra_data_by_instrument.append.py
+++ cal_3_extra_data_by_instrument.append.py
@@ -1,5 +1,3 @@
-import pandas as pd
-
 from setup import *
 from skyrim.whiterun import CCalendar
 
@@ -12,40 +10,44 @@ STP_DATE = sys.argv[1].upper()
 
 calendar_cne = CCalendar(os.path.join(CALENDAR_DIR, "cne_calendar.csv"))
 
-by_trade_date_dfs_list = []
-for trade_date in calendar_cne.get_iter_list(
-        t_bgn_date=(dt.datetime.now() - dt.timedelta(days=20)).strftime("%Y%m%d"),
-        t_stp_date=STP_DATE, t_ascending=True):
-    src_file = "{}.cnf.registered_stock.csv.gz".format(trade_date)
-    src_path = os.path.join(FUTURES_INSTRUMENT_MKT_DATA_DIR, trade_date[0:4], trade_date, src_file)
-    if not os.path.exists(src_path):
-        print("| Warning | registered_stock data for {} does not exist".format(trade_date))
-        continue
+for extra_data_type in ["registered_stock", "basis"]:
+    by_trade_date_dfs_list = []
+    for trade_date in calendar_cne.get_iter_list(
+            t_bgn_date=(dt.datetime.now() - dt.timedelta(days=20)).strftime("%Y%m%d"),
+            t_stp_date=STP_DATE, t_ascending=True):
+        src_file = "{}.cnf.{}.csv.gz".format(trade_date, extra_data_type)
+        src_path = os.path.join(FUTURES_INSTRUMENT_MKT_DATA_DIR, trade_date[0:4], trade_date, src_file)
+        if not os.path.exists(src_path):
+            print("| Warning | {} data for {} does not exist".format(trade_date, extra_data_type))
+            continue
 
-    src_df = pd.read_csv(src_path, dtype="str")
-    src_df["trade_date"] = trade_date
-    by_trade_date_dfs_list.append(src_df)
+        src_df = pd.read_csv(src_path, dtype="str")
+        src_df["trade_date"] = trade_date
+        by_trade_date_dfs_list.append(src_df)
 
-df: pd.DataFrame = pd.concat(by_trade_date_dfs_list, axis=0, ignore_index=True)
-for instrument, instrument_df in df.groupby(by="instrument"):
-    sorted_instrument_df = instrument_df.sort_values(by="trade_date").drop(labels="instrument", axis=1)
+    df: pd.DataFrame = pd.concat(by_trade_date_dfs_list, axis=0, ignore_index=True)
+    for instrument, instrument_df in df.groupby(by="instrument"):
+        if instrument in ["BC.INE"]:
+            continue
 
-    instrument_file = "{}.registered_stock.csv.gz".format(instrument)
-    instrument_path = os.path.join(EXTRA_DATA_DIR, instrument_file)
-    old_sorted_instrument_df = pd.read_csv(instrument_path, dtype=str)
+        sorted_instrument_df = instrument_df.sort_values(by="trade_date").drop(labels="instrument", axis=1)
 
-    new_sorted_instrument_df = pd.concat([
-        old_sorted_instrument_df,
-        sorted_instrument_df
-    ]).drop_duplicates(keep="first")
+        instrument_file = "{}.{}.csv.gz".format(instrument, extra_data_type)
+        instrument_path = os.path.join(EXTRA_DATA_DIR, instrument_file)
+        old_sorted_instrument_df = pd.read_csv(instrument_path, dtype=str)
 
-    print("size before update:{}".format(len(old_sorted_instrument_df)))
-    print("size after  update:{}".format(len(new_sorted_instrument_df)))
-    print(old_sorted_instrument_df.tail())
-    print(new_sorted_instrument_df.tail())
+        new_sorted_instrument_df = pd.concat([
+            old_sorted_instrument_df,
+            sorted_instrument_df
+        ]).drop_duplicates(keep="first")
 
-    new_sorted_instrument_df = new_sorted_instrument_df.sort_values("trade_date", ascending=True)
-    new_sorted_instrument_df = new_sorted_instrument_df.set_index("trade_date")
-    new_sorted_instrument_df.to_csv(instrument_path, index_label="trade_date")
+        print("size before update:{}".format(len(old_sorted_instrument_df)))
+        print("size after  update:{}".format(len(new_sorted_instrument_df)))
+        print(old_sorted_instrument_df.tail())
+        print(new_sorted_instrument_df.tail())
 
-    print("| {} | extra data for {:>6s} are updated".format(dt.datetime.now(), instrument))
+        new_sorted_instrument_df = new_sorted_instrument_df.sort_values("trade_date", ascending=True)
+        new_sorted_instrument_df = new_sorted_instrument_df.set_index("trade_date")
+        new_sorted_instrument_df.to_csv(instrument_path, index_label="trade_date")
+
+        print("| {} | extra data {:>18s} for {:>6s} are updated".format(dt.datetime.now(), extra_data_type, instrument))
--- cal_3_extra_data_by_instrument.py
+++ cal_3_extra_data_by_instrument.py
@@ -11,20 +11,26 @@ STP_DATE = sys.argv[1].upper()
 
 calendar_cne = CCalendar(os.path.join(CALENDAR_DIR, "cne_calendar.csv"))
 
-by_trade_date_dfs_list = []
-for trade_date in calendar_cne.get_iter_list(t_bgn_date=BGN_DATE, t_stp_date=STP_DATE, t_ascending=True):
-    src_file = "{}.cnf.registered_stock.csv.gz".format(trade_date)
-    src_path = os.path.join(FUTURES_INSTRUMENT_MKT_DATA_DIR, trade_date[0:4], trade_date, src_file)
-    if not os.path.exists(src_path):
-        continue
+for extra_data_type in ["registered_stock", "basis"]:
+    by_trade_date_dfs_list = []
+    for trade_date in calendar_cne.get_iter_list(t_bgn_date=BGN_DATE, t_stp_date=STP_DATE, t_ascending=True):
+        src_file = "{}.cnf.{}.csv.gz".format(trade_date, extra_data_type)
+        src_path = os.path.join(FUTURES_INSTRUMENT_MKT_DATA_DIR, trade_date[0:4], trade_date, src_file)
+        if not os.path.exists(src_path):
+            continue
 
-    src_df = pd.read_csv(src_path, dtype="str")
-    src_df["trade_date"] = trade_date
-    by_trade_date_dfs_list.append(src_df)
+        src_df = pd.read_csv(src_path, dtype="str")
+        src_df["trade_date"] = trade_date
+        by_trade_date_dfs_list.append(src_df)
 
-df: pd.DataFrame = pd.concat(by_trade_date_dfs_list, axis=0, ignore_index=True)
-for instrument, instrument_df in df.groupby(by="instrument"):
-    sorted_instrument_df = instrument_df.sort_values(by="trade_date").drop(labels="instrument", axis=1).set_index("trade_date")
-    instrument_file = "{}.registered_stock.csv.gz".format(instrument)
-    instrument_path = os.path.join(EXTRA_DATA_DIR, instrument_file)
-    sorted_instrument_df.to_csv(instrument_path, index_label="trade_date")
+    df: pd.DataFrame = pd.concat(by_trade_date_dfs_list, axis=0, ignore_index=True)
+    for instrument, instrument_df in df.groupby(by="instrument"):
+        if instrument in ["BC.INE"]:
+            continue
+
+        sorted_instrument_df = instrument_df.sort_values(by="trade_date").drop(labels="instrument", axis=1).set_index("trade_date")
+        instrument_file = "{}.{}.csv.gz".format(instrument, extra_data_type)
+        instrument_path = os.path.join(EXTRA_DATA_DIR, instrument_file)
+        sorted_instrument_df.to_csv(instrument_path, index_label="trade_date")
+
+    print("... Extra data: {:>18s} by instrument calculated".format(extra_data_type))
